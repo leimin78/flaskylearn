a7ab10bd0d4630458eeddcdc9b5b552d31bfe608
leimin <18898761417@139.com>
2017-04-13 17:25:23 +0800

    新增表单 forms.py

---
+++ flasky/app/main/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
+
+class NameForm(FlaskForm):
+    name = StringField('你好！',validators=[Required()])
+    submit = SubmitField('提交')
